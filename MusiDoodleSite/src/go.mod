e0d95cc317de641dcc0803b8cd9378be1f1a5674
Jack <JJB9922@protonmail.com>
2024-01-27 20:29:53 +0000

    router muxed

--- MusiDoodleSite/src/go.mod
+++ MusiDoodleSite/src/go.mod
@@ -1,3 +1,5 @@
 module MusiDoodleSite
 
 go 1.21.6
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- MusiDoodleSite/src/main.go
+++ MusiDoodleSite/src/main.go
@@ -3,36 +3,51 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewRouter()
 	fmt.Println("HTTP Server running on 8080!")
 
-	//Serve images from the assets folder.
 	http.Handle("/assets/", http.StripPrefix("/assets/",
 		http.FileServer(http.Dir("../assets"))))
 
-	//Declare a function which deals with http responses.
-	//It parses the index.html file.
-	index := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("../views/index.html",
-			"../views/partials/navbar.tmpl"))
-		tmpl.Execute(w, nil)
-	}
+	http.Handle("/", r)
 
-	screenshots := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("../views/screenshots.html",
-			"../views/partials/navbar.tmpl"))
+	// Define routes
+	r.HandleFunc("/", index)
+	r.HandleFunc("/screenshots", screenshots)
+	r.HandleFunc("/technical", technical)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/download", download)
 
-		tmpl.Execute(w, nil)
-	}
+	http.ListenAndServe(":8080", nil)
+}
 
-	//Handlers
-	http.HandleFunc("/", index)
-	http.HandleFunc("/screenshots", screenshots)
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../views/index.html", "../views/partials/navbar.tmpl"))
+	tmpl.Execute(w, nil)
+}
 
-	//Listen for traffic on 8080, fatal log if there's a problem.
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func screenshots(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../views/screenshots.html", "../views/partials/navbar.tmpl"))
+	tmpl.Execute(w, nil)
+}
+
+func technical(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../views/technical.html", "../views/partials/navbar.tmpl"))
+	tmpl.Execute(w, nil)
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../views/contact.html", "../views/partials/navbar.tmpl"))
+	tmpl.Execute(w, nil)
+}
+
+func download(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("../views/download.html", "../views/partials/navbar.tmpl"))
+	tmpl.Execute(w, nil)
 }
