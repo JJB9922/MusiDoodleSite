dff57df769a9859698db0d988d68b0952622d534
Jack <JJB9922@protonmail.com>
2024-01-27 20:10:20 +0000

    wroking router

--- MusiDoodleSite/src/main.go
+++ MusiDoodleSite/src/main.go
@@ -17,12 +17,21 @@ func main() {
 	//Declare a function which deals with http responses.
 	//It parses the index.html file.
 	index := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("../views/index.html"))
+		tmpl := template.Must(template.ParseFiles("../views/index.html",
+			"../views/partials/navbar.tmpl"))
+		tmpl.Execute(w, nil)
+	}
+
+	screenshots := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("../views/screenshots.html",
+			"../views/partials/navbar.tmpl"))
+
 		tmpl.Execute(w, nil)
 	}
 
 	//Handlers
 	http.HandleFunc("/", index)
+	http.HandleFunc("/screenshots", screenshots)
 
 	//Listen for traffic on 8080, fatal log if there's a problem.
 	log.Fatal(http.ListenAndServe(":8080", nil))
