56550961959371c933aeccd59823c5c7538d3af2
Jack <JJB9922@protonmail.com>
2024-01-27 19:29:55 +0000

    navbar

--- MusiDoodleSite/src/main.go
+++ MusiDoodleSite/src/main.go
@@ -10,20 +10,20 @@ import (
 func main() {
 	fmt.Println("HTTP Server running on 8080!")
 
-  //Serve images from the assets folder.
-	http.Handle("/assets/", http.StripPrefix("/assets/", 
-                          http.FileServer(http.Dir("../assets"))))
+	//Serve images from the assets folder.
+	http.Handle("/assets/", http.StripPrefix("/assets/",
+		http.FileServer(http.Dir("../assets"))))
 
-  //Declare a function which deals with http responses.
-  //It parses the index.html file.
+	//Declare a function which deals with http responses.
+	//It parses the index.html file.
 	index := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("../views/index.html"))
 		tmpl.Execute(w, nil)
 	}
 
-  //Handlers
+	//Handlers
 	http.HandleFunc("/", index)
 
-  //Listen for traffic on 8080, fatal log if there's a problem.
+	//Listen for traffic on 8080, fatal log if there's a problem.
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
