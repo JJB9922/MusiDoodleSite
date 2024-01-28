9767721a0d0c1c4b7ed2114b3dfc82b7b0307238
Jack <JJB9922@protonmail.com>
2024-01-28 01:53:58 +0000

    form stuff

--- MusiDoodleSite/src/main.go
+++ MusiDoodleSite/src/main.go
@@ -23,36 +23,54 @@ func main() {
 	r.HandleFunc("/technical", technical)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/download", download)
+	r.HandleFunc("/submit-contact", contactFormSubmitted).Methods("POST")
 
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../views/index.html", 
-                                            "../views/partials/navbar.tmpl"))
+	tmpl := template.Must(template.ParseFiles("../views/index.html",
+		"../views/partials/navbar.tmpl"))
 	tmpl.Execute(w, nil)
 }
 
 func screenshots(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../views/screenshots.html", 
-                                            "../views/partials/navbar.tmpl"))
+	tmpl := template.Must(template.ParseFiles("../views/screenshots.html",
+		"../views/partials/navbar.tmpl"))
 	tmpl.Execute(w, nil)
 }
 
 func technical(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../views/technical.html", 
-                                            "../views/partials/navbar.tmpl"))
+	tmpl := template.Must(template.ParseFiles("../views/technical.html",
+		"../views/partials/navbar.tmpl"))
 	tmpl.Execute(w, nil)
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../views/contact.html", 
-                                            "../views/partials/navbar.tmpl"))
+	tmpl := template.Must(template.ParseFiles("../views/contact.html",
+		"../views/partials/navbar.tmpl"))
 	tmpl.Execute(w, nil)
 }
 
 func download(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../views/download.html", 
-                                            "../views/partials/navbar.tmpl"))
+	tmpl := template.Must(template.ParseFiles("../views/download.html",
+		"../views/partials/navbar.tmpl"))
+	tmpl.Execute(w, nil)
+}
+
+func contactFormSubmitted(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+
+		fmt.Println("Received contact form submission:")
+		fmt.Println("Email:", r.FormValue("email"))
+		fmt.Println("Subject:", r.FormValue("subject"))
+		fmt.Println("Message:", r.FormValue("message"))
+
+		w.Write([]byte("Form submitted successfully!"))
+		return
+	}
+
+	tmpl := template.Must(template.ParseFiles("../views/screenshots.html",
+		"../views/partials/navbar.tmpl"))
 	tmpl.Execute(w, nil)
 }
